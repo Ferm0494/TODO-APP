e38720b661ffd2f79fc18ac69b40b356b138088d
Ferm0494 <ferm0494@gmail.com>
2020-08-03 15:02:05 -0600

    Create project logic

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 import project from './js/project'
 
-const createProject = () =>{
-  const projectName = document.getElementById("projectNameField").value;
-  const projects = project(projectName).addProject();
-  //populateSelect(projects);
+const createProject = () => {
+
+    const projectName = document.getElementById("projectNameField").value;
+    const lib = project(projectName)
+    populateSelect(lib.projects);
 }
 
-const populateSelect = (arr) =>{
-  const selectProjects = document.getElementById('projects');
-  arr.forEach(element => {
-    const option = document.createElement('option');
-    option.innerHTML = element;  
-    selectProjects.appendChild(option);
-  });
+const populateSelect = (arr) => {
+    console.log(`This is arr ${arr}`)
+    const selectProjects = document.getElementById('projects');
+
+    while (selectProjects.firstChild) {
+        selectProjects.removeChild(selectProjects.firstChild);
+    }
+
+    arr.forEach(element => {
+        const option = document.createElement('option');
+        option.innerHTML = element;
+        selectProjects.appendChild(option);
+    });
 }
 
-document.getElementById('createProject').addEventListener('click', function(e) {
-  e.preventDefault();
-  createProject();
+document.getElementById('createProject').addEventListener('click', function(a) {
+    a.preventDefault();
+    createProject();
 });
+
+// localStorage.clear()
+populateSelect(project().projects)
--- src/js/project.js
+++ src/js/project.js
@@ -1,14 +1,25 @@
-const project = (title) =>{
-  const projects = JSON.parse(localStorage.getItem('projects')) || [];
-  
-  const addProject = () =>{
-    console.log(title);
-/*     projects.push(title);
-    localStorage.setItem('projects',JSON.stringify(projects));
-    return projects; */
-  }
+const project = (title = true) => {
+
+    const projects = JSON.parse(localStorage.getItem('projects')) || [];
+    const addProject = () => {
+        title = title.trim()
+        if (!projects.includes(title) && title !== "" && title !== undefined && title !== true) {
+
+            projects.push(title);
+            console.log(`Adding..${projects}`);
+            localStorage.setItem('projects', JSON.stringify(projects));
+            alert("Project successfully added!")
+        } else if (projects.includes(title)) {
+
+            alert("Element already exists!")
+        } else if (!title || title === "") {
+            alert("Project cant be empty!")
+        }
+    }
+    addProject()
+    return {
+        projects
+    }
 }
-
-
 
 export default project;
